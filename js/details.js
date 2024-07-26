2122c3b23f8b858e5704e1290a010db54e808be9
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-07-26 12:00:16 +0100

    Update details.js

--- js/details.js
+++ js/details.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('anime-image').src = anime.poster;
             document.getElementById('anime-title').textContent = anime.name;
             document.getElementById('anime-description').textContent = anime.description;
-            document.getElementById('watch-link').href = 'watch.html?id=the-rising-of-the-shield-hero-524?ep=10510';
+            document.getElementById('watch-link').href = 'episodes.html?id=${anime.id}';
 
             // Populate more info
             const infoLeft = document.getElementById('more-info-left');
