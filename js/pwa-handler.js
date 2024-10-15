b4b2e967870136270a04c9e72a92376646d1908c
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-10-15 15:25:39 +0100

    Update pwa-handler.js

--- js/pwa-handler.js
+++ js/pwa-handler.js
@@ -12,24 +12,8 @@ if (serviceWorker) {
 // Create a variable to defer the beforeinstallprompt event.
 let beforeInstallEvent;
 
-// Reference the install button from DOM.
-const installButton = document.getElementById("install");
-
 // Watch for the beforeinstallevent and defer it.
 window.addEventListener("beforeinstallprompt", (event) => {
     event.preventDefault();
     beforeInstallEvent = event;
-    installButton.style.display = "block";
-});
-
-// Prompt for Installation when install button is clicked.
-installButton.addEventListener("click", () => {
-    beforeInstallEvent
-        .prompt()
-        .then((choice) => {
-            // Hide the install button as its purpose is over.
-            if (choice.outcome == "accepted") {
-                installButton.style.display = "none";
-            }
-        });
 });
