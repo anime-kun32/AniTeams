d1268e263d921bfa561d4cf6470464dcbfbf3149
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-10-15 10:17:55 +0100

    Create service-worker.js
    Added pwa installation . Meaning that you can install the website as an application on any device .

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+
+// Name of the Cache.
+const CACHE = "cacheV1";
+
+// Select files for caching.
+let urlsToCache = [
+    "/",
+    "/index.html",
+    "/components",
+    "",
+    "/components/images/favicon.png", 
+    "/components/images/clock-face.png",
+    "/js/script.js",
+    "/js/filter.js", 
+    "/js/details.js
+    "/js/pwa-handler.js",
+    "/css/details.css",
+    "/css/style.css",
+    "/css/filter.css"
+   
+];
+
+// Cache all the selected items once application is installed.
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            console.log("Caching started.");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Whenever a resource is requested, return if its cached else fetch the resourcefrom server.
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
