e80186fb5b941e57519190d82ca1569815cb2971
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-01-22 09:23:30 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 // config.js
-const api = "https://aniwatch-api-net.vercel.app/api/v2/hianime/home";  // Your deployed API URL from  https://github.com/ghoshRitesh12/aniwatch-api
-export default api; // Export the API URL
+const api = "https://aniwatch-api-net.vercel.app/api/v2/hianime/home";  // Your deployed API URL
+const details = "https://aniwatch-api-net.vercel.app/api/v2/hianime/anime/";  // Your additional variable
+
+export { api, details };
