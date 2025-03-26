ed2f548126621fa75770cb32f105dbfa699cc6b5
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-03-26 11:15:53 +0000

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const api = "https://aniwatch-api-net.vercel.app/api/v2/hianime/home";  // Your deployed API URL
-const search = "https://aniwatch-api-net.vercel.app/api/v2/hianime/search"; // Search API URL
-const details = "https://aniwatch-api-net.vercel.app/api/v2/hianime/anime";  // Your additional variable
-
-export default api;  // Change to default export
-export { search, details };  // Export search and anotherVariable as named exports
