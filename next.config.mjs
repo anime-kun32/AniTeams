2112fc9967af02d410e900d26592eabb5c79cbce
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-04-20 20:19:02 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.noitatnemucod.net','artworks.thetvdb.com','s4.anilist.co'], // Add your image domain here
+    domains: ['cdn.noitatnemucod.net','artworks.thetvdb.com','s4.anilist.co','https://media.kitsu.app'], // Add your image domain here
   },
 };
 
