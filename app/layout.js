51165b51b37ce5ea9c88ec9d01166350e177eb9e
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-05-07 12:10:43 +0100

    unzipgit

---
+++ app/layout.js
@@ -0,0 +1,40 @@
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import ClientLayoutWrapper from "./components/client/ClientLayoutWrapper";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "AniTeams",
+  description: "Watch anime for free",
+  themeColor: "#000000",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/icons/icon-192x192.png",
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "AniTeams",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+      </body>
+    </html>
+  );
+}
