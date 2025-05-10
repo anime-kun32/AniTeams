2f0468b348006b288f2508e33dc0ac9d4bbbfebb
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-05-10 12:23:21 +0100

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,10 @@
-
 'use client'
 
 import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
-import AuthForm from '../components/AuthForm'
+import Link from 'next/link'
+import AuthForm from '@components/AuthForm'
 
 export default function LoginPage() {
   const router = useRouter()
@@ -15,10 +15,15 @@ export default function LoginPage() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center px-4">
-
-
-        <AuthForm type="login" />
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center px-4 text-white">
+      <AuthForm type="login" />
+      
+      <p className="mt-6 text-sm text-gray-400">
+        No account?{' '}
+        <Link href="/signup" className="text-blue-500 hover:underline">
+          Sign up
+        </Link>
+      </p>
     </div>
   )
 }
